fe3c8c600dfe2538907a7fb825ff8cb76b0f19da
Gonzalo Varela <gonzalo.varela1981@gmail.com>
2019-05-28 14:46:16 -0300

    alter table dominio
    Se cambió la estructurad de la tabla dominio porque el product owner pidió incluir al diccionario codificaciones que están fuera del catalogo y no siguen la misma lógica de normalización.

--- 01.dominio_create_table.sql
+++ 01.dominio_create_table.sql
@@ -1,63 +1,26 @@
---
--- PostgreSQL database dump
---
+-- Table: public.dominio
 
--- Dumped from database version 9.5.7
--- Dumped by pg_dump version 9.6.2
+-- DROP TABLE IF EXISTS public.dominio;
 
--- Started on 2019-05-03 11:38:14
+CREATE TABLE public.dominio
+(
+    codigo character varying(12) COLLATE pg_catalog."default" NOT NULL,
+    etiqueta character varying COLLATE pg_catalog."default" NOT NULL,
+    definicion character varying COLLATE pg_catalog."default",
+    tipo character varying COLLATE pg_catalog."default",
+    codigo_dfdd character(3) COLLATE pg_catalog."default",
+    CONSTRAINT pk_dominio PRIMARY KEY (codigo)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
 
---  Definición de la tabla dominio, contiene los nombres de los campos "dominio" y otras definiciones del Catálogo IGN
+ALTER TABLE public.dominio
+    OWNER to postgres;
 
-SET search_path = public, pg_catalog;
+GRANT ALL ON TABLE public.dominio TO postgres;
 
-SET default_tablespace = '';
+GRANT SELECT ON TABLE public.dominio TO readonly WITH GRANT OPTION;
 
-SET default_with_oids = false;
-
---
--- TOC entry 203 (class 1259 OID 263076)
--- Name: dominio; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE dominio (
-    codigo character(4) NOT NULL,
-    etiqueta character varying NOT NULL,
-    definicion character varying,
-    tipo character varying,
-    codigo_dfdd character(3)
-);
-
-
-ALTER TABLE dominio OWNER TO postgres;
-
---
--- TOC entry 4492 (class 2606 OID 751131)
--- Name: dominio pk_dominio; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY dominio
-    ADD CONSTRAINT pk_dominio PRIMARY KEY (codigo);
-
-
---
--- TOC entry 4624 (class 0 OID 0)
--- Dependencies: 203
--- Name: dominio; Type: ACL; Schema: public; Owner: postgres
---
-
-REVOKE ALL ON TABLE dominio FROM PUBLIC;
-REVOKE ALL ON TABLE dominio FROM postgres;
-GRANT ALL ON TABLE dominio TO postgres;
-GRANT SELECT ON TABLE dominio TO readonly WITH GRANT OPTION;
-SET SESSION AUTHORIZATION readonly;
-GRANT SELECT ON TABLE dominio TO readonly;
-RESET SESSION AUTHORIZATION;
-
-
--- Completed on 2019-05-03 11:38:14
-
---
--- PostgreSQL database dump complete
---
-
+GRANT SELECT ON TABLE public.dominio TO readonly;
